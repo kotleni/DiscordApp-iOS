2745bd994e908602950e0a3573a65128a9ce4220
kotleni <yavarenikya@gmail.com>
2023-03-19 20:06:14 +0200

    Initial Commit

---
+++ DiscordApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiscordApp
+//
+//  Created by Victor Varenik on 19.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
