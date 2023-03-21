3b7d843f06fbdfa21b13e3d2a4c21eefbcd90725
kotleni <yavarenikya@gmail.com>
2023-03-21 14:44:33 +0200

    chore: removed unused nav controller

--- DiscordApp/SceneDelegate.swift
+++ DiscordApp/SceneDelegate.swift
@@ -13,8 +13,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
-        let navController = UINavigationController(rootViewController: MainViewController())
-        window?.rootViewController = navController
+        window?.rootViewController = MainViewController()
         window?.makeKeyAndVisible()
     }
 
