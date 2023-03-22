63acfd497fac5aa56649740e1254c00369c4f136
kotleni <yavarenikya@gmail.com>
2023-03-22 13:05:57 +0200

    chore: added extension for future

---
+++ DiscordApp/Extensions/UIColor.swift
@@ -0,0 +1,41 @@
+//
+//  UIColor.swift
+//  DiscordApp
+//
+//  Created by Victor Varenik on 22.03.2023.
+//
+
+import UIKit
+
+extension UIColor {
+    var hexString: String? {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        var alpha: CGFloat = 0
+
+        let multiplier = CGFloat(255.999999)
+
+        guard self.getRed(&red, green: &green, blue: &blue, alpha: &alpha) else {
+            return nil
+        }
+
+        if alpha == 1.0 {
+            return String(
+                format: "#%02lX%02lX%02lX",
+                Int(red * multiplier),
+                Int(green * multiplier),
+                Int(blue * multiplier)
+            )
+        }
+        else {
+            return String(
+                format: "#%02lX%02lX%02lX%02lX",
+                Int(red * multiplier),
+                Int(green * multiplier),
+                Int(blue * multiplier),
+                Int(alpha * multiplier)
+            )
+        }
+    }
+}
